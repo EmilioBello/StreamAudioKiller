c463648a6d9eac64c1e0b36e6cdd2024b2a60deb
Emilio Bello <>
2019-07-21 18:00:50 +0200

    Moved MainActivity to a new position.

--- app/src/main/java/com/empiezaaprogramar/streamaudiokiller/view/MainActivity.java
+++ app/src/main/java/com/empiezaaprogramar/streamaudiokiller/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.empiezaaprogramar.streamaudiokiller;
+package com.empiezaaprogramar.streamaudiokiller.view;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.empiezaaprogramar.streamaudiokiller.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
